4431f8e5f147f4b38886643583ecac3ab36e1d2b
Pooria Amini <pouriyaxamini@gmail.com>
2025-05-25 15:14:17 +0330

    aspect-ratio custom image supported

--- src/app/_components/customImage.tsx
+++ src/app/_components/customImage.tsx
@@ -4,13 +4,23 @@ export function CustomImage(props: {
   src: string;
   alt: string;
   className?: string;
+  aspectRatio?: number;
   square?: boolean;
 }) {
   const isSquare = "square" in props && props.square !== false;
+  if (isSquare && props.aspectRatio)
+    throw new Error(
+      "Cannot set [props.aspectRatio] and [props.isSquare] in CustomImage at same time."
+    );
+
   return (
     <div
-      className={`relative w-full ${props.className} ${
-        isSquare ? "aspect-square" : ""
+      className={`relative w-full ${props.className ?? ""} ${
+        isSquare
+          ? "aspect-square"
+          : props.aspectRatio
+          ? `aspect-[${props.aspectRatio}]`
+          : ""
       }`}
     >
       <Image src={props.src} fill alt="string" />
