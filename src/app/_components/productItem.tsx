c3cc34031efb927b21d352d0456125bcbe577fde
Pooria Amini <pouriyaxamini@gmail.com>
2025-10-25 13:06:27 +0330

    unify product item layout to display price at bottom of card

--- src/app/_components/productItem.tsx
+++ src/app/_components/productItem.tsx
@@ -7,7 +7,7 @@ export function ProductItem({ product }: { product: ProductDto }) {
   const coverImage = product.media?.find((m) => m.isMain)?.url;
 
   return (
-    <div className="shadow-md rounded-md flex flex-col p-2">
+    <div className="shadow-md rounded-md flex flex-col p-2 gap-1">
       <Link href={`/products/${product.productId}`}>
         <ApiImage
           aspectRatio={256 / 190}
@@ -18,7 +18,7 @@ export function ProductItem({ product }: { product: ProductDto }) {
         <div className="h-px bg-linear-to-r from-transparent via-black opacity-40 to-transparent my-2"></div>
         <h3 className="line-clamp-2 text-sm flex-1">{product.title}</h3>
       </Link>
-      <div className="flex justify-between my-2">
+      <div className="flex justify-between my-2 mt-auto">
         <span>{`$${product.price.toFixed(2)}`}</span>
         <ProductScore score={product.averageScore} />
       </div>
