a1a01dc46b7d183c77ca2f8e6c4c56988fa28e82
Pooria Amini <pouriyaxamini@gmail.com>
2025-10-26 01:30:46 +0330

    define cart reducer for handling cart-base actions

---
+++ src/ui/contexts/cart/cartContext.tsx
@@ -0,0 +1,61 @@
+import { createContext, ReactNode, useContext, useReducer } from "react";
+import { CartPayloads, cartReducer, CartState } from "./cartReducer";
+import { NoContextDefinedError } from "@/ui/errors/NoContextDefinedError";
+
+interface ICartContext extends CartState {
+  actions: {
+    add(payload: CartPayloads<"Add">): void;
+    remove(payload: CartPayloads<"Remove">): void;
+    decrease(payload: CartPayloads<"Decrease">): void;
+  };
+}
+
+const CartContext = createContext<ICartContext>({} as unknown as ICartContext);
+
+export function CartHandlerProvider({ children }: { children: ReactNode }) {
+  const _context = useCart();
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: [],
+  });
+
+  if (_context) {
+    throw new Error("Cart context must define only one time in component tree");
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        ...state,
+        actions: {
+          add(payload) {
+            dispatch({
+              action: "Add",
+              payload,
+            });
+          },
+          decrease(payload) {
+            dispatch({
+              action: "Decrease",
+              payload,
+            });
+          },
+          remove(payload) {
+            dispatch({ action: "Remove", payload });
+          },
+        },
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new NoContextDefinedError("Cart");
+  }
+
+  return context;
+}
---
+++ src/ui/contexts/cart/cartReducer.tsx
@@ -0,0 +1,82 @@
+import { ProductDto } from "@/core/Dtos/ProductDto";
+
+interface AddProductData {
+  product: ProductDto;
+  amount?: number;
+}
+
+interface CartActionPayloads {
+  Add: AddProductData;
+  Decrease: ProductDto;
+  Remove: void;
+}
+
+export type CartActions = keyof CartActionPayloads;
+export type CartPayloads<T extends CartActions> = CartActionPayloads[T];
+
+export interface CartState {
+  products: {
+    product: ProductDto;
+    amount: number;
+  }[];
+}
+
+export function cartReducer<T extends CartActions>(
+  state: CartState,
+  action: { action: T; payload: CartPayloads<T> }
+): CartState {
+  switch (action.action) {
+    case "Add": {
+      const addedData = action.payload as AddProductData;
+      if (!addedData || !addedData.product) {
+        throw new Error("no product state provided for Cart Reducer");
+      }
+      const productInList = state.products.find(
+        (p) => p.product.productId == addedData.product.productId
+      );
+
+      const addedAmount = addedData.amount ?? 1;
+
+      if (productInList) {
+        productInList.amount += addedAmount;
+
+        return { ...state };
+      }
+
+      state.products.push({
+        product: addedData.product,
+        amount: addedAmount,
+      });
+
+      return { ...state };
+    }
+    case "Decrease": {
+      const addedData = action.payload as AddProductData;
+      if (!addedData || !addedData.product) {
+        throw new Error("no product state provided for Cart Reducer");
+      }
+      const productIndexInList = state.products.findIndex(
+        (p) => p.product.productId == addedData.product.productId
+      );
+
+      if (productIndexInList < 0) {
+        return state;
+      }
+
+      const productInList = state.products[productIndexInList];
+
+      if (!--productInList.amount) {
+        state.products.splice(productIndexInList, 1);
+      }
+
+      return {
+        ...state,
+      };
+    }
+    case "Remove": {
+      return {
+        products: [],
+      };
+    }
+  }
+}
