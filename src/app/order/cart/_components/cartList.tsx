3c6c71c8abfb574d0eb03f5d951e300f5b4ad7a2
Pooria Amini <pouriyaxamini@gmail.com>
2025-10-27 19:17:58 +0330

    handle empty cart list page

--- src/app/order/cart/_components/cartList.tsx
+++ src/app/order/cart/_components/cartList.tsx
@@ -10,6 +10,26 @@ export function CartList() {
   const { items } = useCart();
   const cartItems = items.map((item) => new CI(item.product, item.amount));
   const cartPrice = cartItems.reduce((prev, cur) => prev + cur.finalPrice, 0);
+
+  if (!items.length) {
+    return (
+      <div className="my-10 bg-slate-300 rounded-lg p-8 flex flex-col gap-4 justify-center items-center">
+        <h4 className="text-2xl font-semibold text-orange-500">
+          Cart is empty
+        </h4>
+        <p className="">
+          Go to{" "}
+          <Link
+            className="font-bold text-primary-blue-0c underline"
+            href={{ pathname: "/products" }}
+          >
+            Products
+          </Link>{" "}
+          page to add some items to your cart
+        </p>
+      </div>
+    );
+  }
   return (
     <div className="mt-12">
       {cartItems.map((item, index) => (
