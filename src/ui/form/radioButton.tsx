cdd6b400d12f7ab0938623f35cf07b78e3cf8ad5
Pooria Amini <pouriyaxamini@gmail.com>
2025-10-27 22:15:13 +0330

    simplify radio button layout

--- src/ui/form/radioButton.tsx
+++ src/ui/form/radioButton.tsx
@@ -1,46 +1,67 @@
 "use client";
 import { ReactNode } from "react";
 
-export function RadioButton({
-  label,
-  name,
-  onSelect,
-  value,
-}: {
-  label: string | ReactNode;
+interface RadioButtonInputProps {
   name: string;
   value?: unknown;
   onSelect?: (options: { name: string; value: unknown }) => void;
-}) {
+  onChange?: (state: boolean) => void;
+  size?: "big" | "normal";
+}
+
+export function RadioButton({
+  label,
+  ...props
+}: {
+  label: string | ReactNode;
+} & RadioButtonInputProps) {
   return (
-    <label className="text-gray-neutral-44">
-      <table>
-        <tbody>
-          <tr>
-            <td className="align-baseline">
-              <input
-                hidden
-                className="
+    <label className="flex">
+      <div className="self-baseline">
+        <RadioButtonInput {...props} />
+      </div>
+      {label}
+    </label>
+  );
+}
+
+function RadioButtonInput({
+  name,
+  onSelect,
+  size,
+  value,
+}: RadioButtonInputProps) {
+  return (
+    <>
+      <input
+        hidden
+        className="
                 [&:checked+span]:border-primary-blue-400 [&:checked+span]:before:block 
                [&:checked+span]:after:block"
-                type="radio"
-                name={name}
-                onChange={() => {
-                  onSelect?.({ name, value });
-                }}
-              />
-              <span
-                className="
-                align-middle me-1 inline-block relative items-center border-[2px] w-[16px] 
+        type="radio"
+        name={name}
+        onChange={() => {
+          onSelect?.({ name, value });
+        }}
+      />
+      <span
+        className={`
+                align-middle me-1 inline-block relative items-center border-[2px] ${
+                  size == "normal" ? "w-[16px] " : "w-[20px] "
+                }
                 aspect-square rounded-full border-gray-neutral-44  
                
                 before:hidden
-                before:w-[8px] before:aspect-square before:absolute
+                ${
+                  size == "normal" ? "before:w-[8px]" : "before:w-[12px]"
+                } before:aspect-square before:absolute
                 before:top-1/2 before:left-1/2  before:-translate-1/2
                 before:bg-primary-blue-400 before:rounded-full
                 
                 after:hidden
-                after:w-[16px] after:aspect-square after:absolute
+                ${
+                  size == "normal" ? "after:w-[16px]" : "after:w-[20px]"
+                } after:aspect-square after:absolute
                 after:top-1/2 after:left-1/2  after:-translate-1/2
                 after:rounded-full
                 after:bg-sky-400
@@ -48,15 +69,8 @@ export function RadioButton({
                 after:animate-ping
                 after:[animation-iteration-count:1]
                 after:[animation-fill-mode:forwards]
-                "
-              ></span>
-            </td>
-            <td>
-              <span>{label}</span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </label>
+                `}
+      ></span>
+    </>
   );
 }
