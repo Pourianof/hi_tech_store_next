6d8a9cbe269604ef90bea51d110e69885de948bc
Pooria Amini <pouriyaxamini@gmail.com>
2025-12-05 06:34:52 +0330

    define redirector component to delegate a delayed redirection to a client component

---
+++ src/ui/redirector.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface RedirectorProps {
+  timeout: number;
+  push?: boolean;
+  destinationPath: string;
+}
+
+export function useRedirect({
+  timeout = 1000,
+  push,
+  destinationPath,
+}: RedirectorProps) {
+  const router = useRouter();
+
+  setTimeout(() => {
+    if (push) {
+      router.push(destinationPath);
+    } else {
+      router.replace(destinationPath);
+    }
+  }, timeout);
+}
+
+export function Redirector(props: RedirectorProps) {
+  useRedirect(props);
+  return null;
+}
