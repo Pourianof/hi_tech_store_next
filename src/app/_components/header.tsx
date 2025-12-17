c0c0275a49e920e56ab9822d42b1ec7be22dee93
Pooria Amini <pouriyaxamini@gmail.com>
2025-12-17 21:51:25 +0330

    depend live on session state instead of static one

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,26 +1,24 @@
 import Icon from "@/ui/icons/icon";
-import { Session } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
-import { auth } from "../../../auth";
 import { Wrapper } from "../_shared/wrapper";
 import { APP_TITLE } from "../consts";
 import { CartBadge } from "./cartBadge";
 import { HeaderDrawerButton } from "./headerDrawerButton";
+import { UserLink } from "./userLink";
 
 export async function Header() {
-  const session = await auth();
   return (
     <header className="border-b py-2 sticky top-0 bg-white z-50">
       <Wrapper>
-        <LargeHeader session={session} />
-        <MobileHeader session={session} />
+        <LargeHeader />
+        <MobileHeader />
       </Wrapper>
     </header>
   );
 }
 
-function MobileHeader({ session }: { session: Session | null }) {
+function MobileHeader() {
   return (
     <div className="md:hidden flex justify-between gap-2 text-xl font-medium">
       <HeaderDrawerButton />
@@ -29,13 +27,13 @@ function MobileHeader({ session }: { session: Session | null }) {
       </Link>
       <div className="flex gap-4 items-center">
         <CartBadge />
-        <UserLink session={session} />
+        <UserLink />
       </div>
     </div>
   );
 }
 
-function LargeHeader({ session }: { session: Session | null }) {
+function LargeHeader() {
   return (
     <div className="items-center hidden md:flex ">
       <Image alt={APP_TITLE} src={"/icons/logo.svg"} width={50} height={50} />
@@ -49,24 +47,8 @@ function LargeHeader({ session }: { session: Session | null }) {
       <div className="space-x-2 flex">
         <Icon name="search" />
         <CartBadge />
-        <UserLink session={session} />
+        <UserLink />
       </div>
     </div>
-  );
-}
-
-function UserLink({ session }: { session: Session | null }) {
-  return (
-    <Link
-      className="flex gap-1"
-      href={{ pathname: session ? "/account" : "/login" }}
-    >
-      <Icon name="user" />
-      {!!session && (
-        <span className="text-sm bg-gray-300 inline-block px-1 rounded hover:bg-gray-400">
-          {session.user?.name}
-        </span>
-      )}
-    </Link>
   );
 }
---
+++ src/app/_components/userLink.tsx
@@ -0,0 +1,21 @@
+"use client";
+import Icon from "@/ui/icons/icon";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+
+export function UserLink() {
+  const { data: session } = useSession();
+  return (
+    <Link
+      className="flex gap-1"
+      href={{ pathname: session ? "/account" : "/login" }}
+    >
+      <Icon name="user" />
+      {!!session && (
+        <span className="text-sm bg-gray-300 inline-block px-1 rounded hover:bg-gray-400">
+          {session.user?.name}
+        </span>
+      )}
+    </Link>
+  );
+}
