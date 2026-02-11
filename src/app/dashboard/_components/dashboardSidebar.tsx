0ffc1aeeb1d5c189ea84e4b5f025711789c868ee
Pooria Amini <pouriyaxamini@gmail.com>
2026-02-11 22:29:11 +0330

    add discount item to dashboard sidebar

--- src/app/dashboard/_components/dashboardSidebar.tsx
+++ src/app/dashboard/_components/dashboardSidebar.tsx
@@ -1,44 +1,104 @@
-import Icon from "@/ui/icons/icon";
+import Icon, { IconNames } from "@/ui/icons/icon";
 import Link from "next/link";
 import { auth } from "../../../../auth";
 
 export default async function DashboardSidebar() {
-  const session = await auth();
-
   return (
     <div>
-      <h3 className="my-1">
-        <Icon className="text-4xl me-1" name="profile" />
-        Hello{" "}
-        <span className="bg-gray-300 inline-block rounded py-0.5 px-1">
-          {session?.user.name} {session?.user.lastName}
-        </span>
-      </h3>
+      <SidebarHeader />
       <ul className="bg-gray-300 p-4 [&_li]:my-1.5 rounded">
         <li className="font-semibold">
           <Link href="/dashboard">Overview</Link>
         </li>
-        <li>
-          <Icon className="me-1" name="product" />
-          <span className="text-gray-800 cursor-default font-semibold">
-            Products
-          </span>
-          <div className="ps-2">
-            <ul className="px-2 space-y-1 border-l border-l-black/50 text-blue-950">
-              <li>
-                <Link href="/dashboard/add-product">Add new product</Link>
-              </li>
-              <li>
-                <Link href="/dashboard/my-products">My products</Link>
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li className="font-semibold">
-          <Icon className="me-1" name="category" />
-          <Link href="/dashboard/categories">Categories</Link>
-        </li>
+        <SidebarProductItem />
+        <SidebarCategoryItem />
+        <SidebarDiscountItem />
       </ul>
     </div>
+  );
+}
+
+async function SidebarHeader() {
+  const session = await auth();
+
+  return (
+    <h3 className="my-1">
+      <Icon className="text-4xl me-1" name="profile" />
+      Hello{" "}
+      <span className="bg-gray-300 inline-block rounded py-0.5 px-1">
+        {session?.user.name} {session?.user.lastName}
+      </span>
+    </h3>
+  );
+}
+
+function SidebarProductItem() {
+  return (
+    <SidebarItem
+      title="Products"
+      iconName="product"
+      subList={[
+        {
+          title: "Add new product",
+          path: "/dashboard/add-product",
+        },
+        {
+          title: "My products",
+          path: "/dashboard/my-products",
+        },
+      ]}
+    />
+  );
+}
+
+function SidebarCategoryItem() {
+  return (
+    <SidebarItem
+      title="Categories"
+      iconName="category"
+      link="/dashboard/categories"
+    />
+  );
+}
+
+function SidebarDiscountItem() {
+  return (
+    <SidebarItem
+      title="Discounts"
+      iconName="discount"
+      link="/dashboard/discount-panel"
+    />
+  );
+}
+
+function SidebarItem({
+  title,
+  link,
+  iconName,
+  subList,
+}: {
+  title: string;
+  iconName: IconNames;
+  link?: string;
+  subList?: { title: string; path: string }[];
+}) {
+  return (
+    <li>
+      <Icon className="me-1" name={iconName} />
+      <span className="text-gray-800 cursor-default font-semibold">
+        {!!link ? <Link href={link}>{title}</Link> : title}
+      </span>
+      {!!subList && subList.length > 0 && (
+        <div className="ps-2">
+          <ul className="px-2 space-y-1 border-l border-l-black/50 text-blue-950">
+            {subList.map((item) => (
+              <li key={item.title}>
+                <Link href={item.path}>{item.title}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </li>
   );
 }
