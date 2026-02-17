c06c20c10f0318bbbb89604c9cb48cfbd39487eb
Pooria Amini <pouriyaxamini@gmail.com>
2026-02-18 02:06:06 +0330

    make modal detect overflow

--- src/ui/modal/modal.tsx
+++ src/ui/modal/modal.tsx
@@ -1,25 +1,27 @@
 "use client";
+import { useRouter } from "next/navigation";
 import {
   createContext,
   ReactNode,
   useCallback,
   useContext,
   useLayoutEffect,
+  useRef,
+  useState,
 } from "react";
 import { createPortal } from "react-dom";
 import { useBackBtnHandler } from "../hooks/useBackBtnHandler";
 import { MODAL_CONTAINER_ID } from "./modalContainer";
-import { useRouter } from "next/navigation";
 
 interface IModalContextState {
   isBackBtnHandled: boolean;
 }
 
 const ModalContext = createContext<IModalContextState>(
-  {} as unknown as IModalContextState
+  {} as unknown as IModalContextState,
 );
 
-type ModalVariants = "full-page" | "center" | "standard" | "raw";
+type ModalVariants = "full-page" | "center" | "standard" | "raw" | "center-x";
 type RequiredOnClose = { onClose: VoidFunction };
 export function Modal<TBack extends boolean, TVariant extends ModalVariants>({
   children,
@@ -41,9 +43,12 @@ export function Modal<TBack extends boolean, TVariant extends ModalVariants>({
 } & (TBack extends true
   ? RequiredOnClose
   : TVariant extends "full-page"
-  ? RequiredOnClose
-  : Partial<RequiredOnClose>)) {
+    ? RequiredOnClose
+    : Partial<RequiredOnClose>)) {
   const isBackBtnHandled = backBtnHandling || variants == "full-page";
+
+  const [hasOverflowed, setHasOverflowed] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
 
   const backHandler = useCallback(() => {
     if (isBackBtnHandled) {
@@ -65,13 +70,21 @@ export function Modal<TBack extends boolean, TVariant extends ModalVariants>({
     }
   }, [diableScroll]);
 
+  useLayoutEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+    const diff = window.innerHeight - container.scrollHeight;
+    const hasOverflow = diff < 0 || diff / window.innerHeight < 0.05;
+    setHasOverflowed(hasOverflow);
+  }, [children]);
+
   let overlayClassName;
   let _containerClassName = "";
 
   switch (variants) {
     case "center":
       overlayClassName = "items-center justify-center";
-      _containerClassName = "rounded-2xl max-w-sm w-full shadow-xl";
+      _containerClassName = "rounded-2xl shadow-xl";
       break;
     case "full-page":
       overlayClassName = "items-stretch";
@@ -80,11 +93,16 @@ export function Modal<TBack extends boolean, TVariant extends ModalVariants>({
 
     case "standard":
       overlayClassName = "items-center";
-      _containerClassName = "rounded-2xl max-w-sm w-full shadow-xl";
+      _containerClassName = "rounded-2xl shadow-xl";
+      break;
+    case "center-x":
+      overlayClassName = "justify-center";
+      _containerClassName = "rounded-2xl shadow-xl";
       break;
     case "raw":
       _containerClassName = "";
   }
+
   return createPortal(
     <ModalContext.Provider value={{ isBackBtnHandled: isBackBtnHandled }}>
       <div
@@ -103,6 +121,7 @@ export function Modal<TBack extends boolean, TVariant extends ModalVariants>({
           }}
         ></div>
         <div
+          ref={containerRef}
           className={
             "bg-white z-20 " +
             (!noPadding ? " p-6 " : "") +
@@ -110,12 +129,21 @@ export function Modal<TBack extends boolean, TVariant extends ModalVariants>({
             " " +
             (containerClassName ?? "")
           }
+          style={
+            hasOverflowed
+              ? {
+                  position: "absolute",
+                  top: "10dvh",
+                  bottom: "5dvh",
+                }
+              : undefined
+          }
         >
           {children}
         </div>
       </div>
     </ModalContext.Provider>,
-    document.getElementById(MODAL_CONTAINER_ID) || document.body
+    document.getElementById(MODAL_CONTAINER_ID) || document.body,
   );
 }
 
