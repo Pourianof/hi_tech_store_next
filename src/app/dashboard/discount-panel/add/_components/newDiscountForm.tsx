e840aa553937d8d93495bca4a672335d8b061799
Pooria Amini <pouriyaxamini@gmail.com>
2026-02-24 22:36:49 +0330

    remove test initial default values

--- src/app/dashboard/discount-panel/add/_components/newDiscountForm.tsx
+++ src/app/dashboard/discount-panel/add/_components/newDiscountForm.tsx
@@ -3,11 +3,10 @@
 import { DiscountCode, discountCodeSchema } from "@/core/models/discount";
 import { submitDiscountCodeAction } from "@/lib/server_actions/discountActions";
 import { StatefulForm } from "@/ui/form/statefulForm";
+import { useRouter } from "next/navigation";
 import { ReactNode } from "react";
 import toast from "react-hot-toast";
-import { DateObject } from "react-multi-date-picker";
 import { SubmitButton } from "./submitButton";
-import { useRouter } from "next/navigation";
 
 export function NewDiscountForm({ children }: { children: ReactNode }) {
   const router = useRouter();
@@ -37,19 +36,13 @@ export function NewDiscountForm({ children }: { children: ReactNode }) {
       }}
       defaultValues={
         {
-          description: "test description",
-          startTime: new DateObject().subtract(5, "d").toUnix() * 1000,
-          endTime: new DateObject().add(5, "d").toUnix() * 1000,
           rules: [
             {
-              description: "Some test description",
-              name: "some test name",
               conditions: [
                 {
                   conditions: [{}],
                 },
               ],
-              discountAction: { type: 0, value: 10 },
             },
           ],
         } as Partial<DiscountCode>
