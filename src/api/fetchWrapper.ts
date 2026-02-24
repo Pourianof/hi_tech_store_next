baa363cde269881cbfc94bfbf78ff01e067cca56
Pooria Amini <pouriyaxamini@gmail.com>
2026-02-24 18:30:08 +0330

    design fetch wrapper for simplify fetching

---
+++ src/api/fetchWrapper.ts
@@ -0,0 +1,72 @@
+import { ResultModel } from "@/core/models/resultModel";
+import { auth } from "../../auth";
+
+async function get<T>(url: string) {
+  const response = await fetch(url, {
+    method: "GET",
+    headers: await getHeaders(),
+  });
+
+  return handleResponse<T>(response);
+}
+
+async function handleResponse<T>(response: Response) {
+  let json: unknown;
+  try {
+    json = await response.json();
+  } catch {}
+
+  return {
+    status: response.ok ? "success" : "failed",
+    statusCode: response.status,
+    data: json
+      ? (json as T)
+      : !response.ok
+        ? { title: response.statusText }
+        : undefined,
+  } as ResultModel<T>;
+}
+
+async function post<T>(url: string, data: object) {
+  const isFormData = data instanceof FormData;
+  return handleResponse<T>(
+    await fetch(url, {
+      method: "POST",
+      headers: await getHeaders(),
+      body: isFormData ? data : JSON.stringify(data),
+    }),
+  );
+}
+
+async function put<T>(url: string, data: object) {
+  const isFormData = data instanceof FormData;
+  return handleResponse<T>(
+    await fetch(url, {
+      method: "PUT",
+      headers: await getHeaders(),
+      body: isFormData ? data : JSON.stringify(data),
+    }),
+  );
+}
+
+async function _delete<T>(url: string) {
+  return handleResponse<T>(
+    await fetch(url, {
+      method: "DELETE",
+      headers: await getHeaders(),
+    }),
+  );
+}
+
+async function getHeaders() {
+  const session = await auth();
+  const headers = new Headers();
+
+  if (session) {
+    headers.set("Authorization", `Bearer ${session.apiToken}`);
+  }
+
+  return headers;
+}
+
+export const fetchWrapper = { get, post, put, delete: _delete };
