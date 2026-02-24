88f5fdfd5fed826631d856aa19300d91fff55f6f
Pooria Amini <pouriyaxamini@gmail.com>
2026-02-24 17:57:09 +0330

    add error label component

--- src/ui/form/datePicker.tsx
+++ src/ui/form/datePicker.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { Controller } from "react-hook-form";
 import DatePicker, { CalendarProps, DateObject } from "react-multi-date-picker";
+import { Column } from "../layouts/column";
+import { ErrorMessageLabel } from "./errorMessageLabel";
 
 type Props = {
   fieldname: string;
@@ -10,21 +12,24 @@ type Props = {
 const DATE_PICKER_CLASS = "p-1 px-1.5 border border-stone-600 rounded w-40";
 export function AppDatePicker({ fieldname, dateModifier, ...props }: Props) {
   return (
-    <Controller
-      name={fieldname}
-      render={({ field: { onChange, value } }) => (
-        <DatePicker
-          value={value}
-          onChange={(date) =>
-            date
-              ? onChange((dateModifier?.(date) ?? date)?.toUnix() * 1000)
-              : undefined
-          }
-          inputClass={DATE_PICKER_CLASS}
-          {...props}
-        />
-      )}
-    />
+    <Column>
+      <Controller
+        name={fieldname}
+        render={({ field: { onChange, value } }) => (
+          <DatePicker
+            value={value}
+            onChange={(date) =>
+              date
+                ? onChange((dateModifier?.(date) ?? date)?.toUnix() * 1000)
+                : undefined
+            }
+            inputClass={DATE_PICKER_CLASS}
+            {...props}
+          />
+        )}
+      />
+      <ErrorMessageLabel fieldName={fieldname} />
+    </Column>
   );
 }
 
