9c20d494786d51b2894f6af998f51c3c5fb0b224
Pooria Amini <pouriyaxamini@gmail.com>
2026-04-12 22:47:47 +0330

    disable preview button mode added

--- src/app/dashboard/discount-panel/_components/discountScriptEditor.tsx
+++ src/app/dashboard/discount-panel/_components/discountScriptEditor.tsx
@@ -2,12 +2,12 @@
 
 import { ProductItem } from "@/app/_components/productItem";
 import { ProductModel } from "@/core/models/productModel";
-import { checkDiscountScriptAction } from "@/lib/server_actions/discountActions";
 import { FilledButton } from "@/ui/form/AppButtons";
 import Icon from "@/ui/icons/icon";
 import { Column } from "@/ui/layouts/column";
 import { Row } from "@/ui/layouts/row";
 import { Modal } from "@/ui/modal/modal";
+import { discountActions } from "@/ui/server_actions_wrapper/discountActions";
 import { CircularProgress, IconButton } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Controller } from "react-hook-form";
@@ -17,39 +17,46 @@ export function DiscountScriptEditor({
   readOnly,
   fieldname,
   script,
+  noPreview,
 }: {
   readOnly?: boolean;
   fieldname?: string;
   script?: string;
+  noPreview?: boolean;
 }) {
   const [displayPreviewModal, setDisplayPreviewModal] = useState(false);
+
+  const inputClassname = [
+    "p-4 outline-0 border border-gray-400",
+    noPreview ? "w-full" : "w-3/4",
+  ].join(" ");
 
   if (!fieldname) {
     return (
       <Row>
-        <input
-          className="p-4 outline-0 border border-gray-400 w-3/4"
-          value={script}
-          readOnly={true}
-        />
-        <FilledButton
-          noFullWidth
-          className="w-1/4"
-          onClick={async () => {
-            if (!script?.trim()) {
-              toast.error("Script is empty");
-              return;
-            }
-            setDisplayPreviewModal(true);
-          }}
-        >
-          Preview script result
-        </FilledButton>
-        {displayPreviewModal && (
-          <DiscountScriptResultPreview
-            script={script!}
-            onClose={() => setDisplayPreviewModal(false)}
-          />
+        <input className={inputClassname} value={script} readOnly={true} />
+        {!noPreview && (
+          <>
+            <FilledButton
+              noFullWidth
+              className="w-1/4"
+              onClick={async () => {
+                if (!script?.trim()) {
+                  toast.error("Script is empty");
+                  return;
+                }
+                setDisplayPreviewModal(true);
+              }}
+            >
+              Preview script result
+            </FilledButton>
+            {displayPreviewModal && (
+              <DiscountScriptResultPreview
+                script={script!}
+                onClose={() => setDisplayPreviewModal(false)}
+              />
+            )}
+          </>
         )}
       </Row>
     );
@@ -63,26 +70,28 @@ export function DiscountScriptEditor({
         <Column className="gap-2">
           <Row>
             <input
-              className="p-4 outline-0 border border-gray-400 w-3/4"
+              className={inputClassname}
               value={script}
               readOnly={readOnly}
               onChange={(e) => {
                 onChange((e.target as HTMLInputElement).value);
               }}
             />
-            <FilledButton
-              noFullWidth
-              className="w-1/4"
-              onClick={async () => {
-                if (!script.trim()) {
-                  toast.error("Script is empty");
-                  return;
-                }
-                setDisplayPreviewModal(true);
-              }}
-            >
-              Preview script result
-            </FilledButton>
+            {!noPreview && (
+              <FilledButton
+                noFullWidth
+                className="w-1/4"
+                onClick={async () => {
+                  if (!script.trim()) {
+                    toast.error("Script is empty");
+                    return;
+                  }
+                  setDisplayPreviewModal(true);
+                }}
+              >
+                Preview script result
+              </FilledButton>
+            )}
           </Row>
           {displayPreviewModal && (
             <DiscountScriptResultPreview
@@ -109,9 +118,10 @@ function DiscountScriptResultPreview({
   useEffect(() => {
     setIsLoading(true);
 
-    checkDiscountScriptAction({
-      script,
-    })
+    discountActions
+      .checkDiscountScript({
+        script,
+      })
       .then((result) => {
         if (result.status == "failed") {
           toast.error(result.data.title);
