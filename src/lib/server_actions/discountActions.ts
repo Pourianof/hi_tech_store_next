a6e1df9c0fa73940b5ec9eacce18a0e890b6f1ac
Pooria Amini <pouriyaxamini@gmail.com>
2026-04-12 22:45:04 +0330

    left from actions wrapper chnages

--- src/lib/server_actions/discountActions.ts
+++ src/lib/server_actions/discountActions.ts
@@ -15,9 +15,6 @@ import {
   DiscountCreationDto,
 } from "@/core/Dtos/discountCodeDto";
 import { workWithSession } from "../helpers/sessionHelper";
-import { DiscountConditionScriptCheckDto } from "@/core/Dtos/discountDto";
-import { ProductModel } from "@/core/models/productModel";
-import { ResultModel } from "@/core/models/resultModel";
 
 export async function getDiscountEntitiesAction() {
   return getDiscountEntities();
@@ -48,14 +45,4 @@ export async function getAllDiscountsAction(query?: DiscountCodeQuery) {
 }
 
 export const updateDiscountAction = updateDiscountApi;
-export const checkDiscountScriptAction = async (
-  scriptDto: DiscountConditionScriptCheckDto,
-) => {
-  const result = await checkDiscountScriptApi(scriptDto);
-
-  if (result.status == "success") {
-    result.data = result.data.map((p) => ProductModel.CreateWith(p));
-  }
-
-  return result as ResultModel<ProductModel[]>;
-};
+export const checkDiscountScriptAction = checkDiscountScriptApi;
--- src/lib/server_actions/productActions.ts
+++ src/lib/server_actions/productActions.ts
@@ -2,8 +2,6 @@
 import { createNewProduct, getColors, getProducts } from "@/api/productApi";
 import { ResultModel } from "@/core/models/resultModel";
 import { workWithSession } from "../helpers/sessionHelper";
-import { PagedResults } from "@/core/Dtos/pagedResult";
-import { ProductModel } from "@/core/models/productModel";
 
 export async function createProduct(product: FormData): Promise<ResultModel> {
   return workWithSession((session) => {
@@ -15,16 +13,4 @@ export async function getProductColorsAction() {
   return getColors();
 }
 
-export async function getProductsAction(
-  searchQueries?: Record<string, string>,
-): Promise<ResultModel<PagedResults<ProductModel>>> {
-  const result = await getProducts(searchQueries);
-
-  if (result.status == "success") {
-    result.data.items = result.data.items.map((p) =>
-      ProductModel.CreateWithDto(p),
-    );
-  }
-
-  return result as ResultModel<PagedResults<ProductModel>>;
-}
+export const getProductsAction = getProducts;
