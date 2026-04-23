746ef841149dc9b0c8ebf351d08f319eed5bd053
Pooria Amini <pouriyaxamini@gmail.com>
2026-04-23 12:42:25 +0330

    fix errored fetching and improve caching

--- src/api/apiServerMisc.ts
+++ src/api/apiServerMisc.ts
@@ -1,11 +1,30 @@
 import { apiRoutes } from "./apiRoutes";
 
 const HEALTHY_STATE = "Healthy";
+let lastCheckTime = 0;
+let cachedState: boolean | undefined = undefined;
+const CACHE_INTERVAL = process.env.NODE_ENV == "development" ? 0 : 10 * 1000;
 
 export async function isHealthyApi() {
-  const result = await fetch(apiRoutes.healthCheck);
+  const now = Date.now();
 
-  const state = await result.text();
+  if (
+    typeof cachedState != "undefined" &&
+    now - lastCheckTime < CACHE_INTERVAL
+  ) {
+    return cachedState;
+  }
 
-  return HEALTHY_STATE.toLowerCase() == state.toLowerCase();
+  try {
+    const result = await fetch(apiRoutes.healthCheck);
+
+    const state = await result.text();
+    cachedState = HEALTHY_STATE.toLowerCase() == state.toLowerCase();
+    return cachedState;
+  } catch {
+    cachedState = false;
+    return cachedState;
+  } finally {
+    lastCheckTime = Date.now();
+  }
 }
