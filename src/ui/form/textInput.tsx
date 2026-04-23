f149853cba63ad16085aa0e1f7d88ad60a82e1ec
Pooria Amini <pouriyaxamini@gmail.com>
2026-04-23 14:20:06 +0330

    improve some border style

--- src/ui/form/textInput.tsx
+++ src/ui/form/textInput.tsx
@@ -4,12 +4,12 @@ export function TextInput(
   props: DetailedHTMLProps<
     InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
-  >
+  >,
 ) {
   return (
     <input
       {...props}
-      className={`border p-2 rounded w-full ${props.className ?? ""}`}
+      className={`border p-2 rounded w-full outline-0 focus:border-black ${props.className ?? ""}`}
     />
   );
 }
