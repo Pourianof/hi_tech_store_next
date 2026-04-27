2488f25fc1890ce0ed2c887d56be3e0dbb8a51d4
Pooria Amini <pouriyaxamini@gmail.com>
2026-04-27 19:37:02 +0330

    increase server action size constraint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10MB",
+    },
+  },
   turbopack: {
     rules: {
       "./src/assets/**/*.svg": {
