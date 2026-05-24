3ec252b063dd5e3cd968573a2c03717de0602187
Pooria Amini <pouriyaxamini@gmail.com>
2026-05-24 18:03:07 +0330

    improve user link

--- src/app/_components/userLink.tsx
+++ src/app/_components/userLink.tsx
@@ -1,20 +1,42 @@
 "use client";
+import { useAuth } from "@/ui/contexts/authContext";
 import Icon from "@/ui/icons/icon";
-import { useSession } from "next-auth/react";
+import { ApiImage } from "@/ui/image/ApiImage";
+import { Column } from "@/ui/layouts/column";
+import { Caption } from "@/ui/theme/text/caption";
+import { CircularProgress } from "@mui/material";
 import Link from "next/link";
 
 export function UserLink() {
-  const { data: session } = useSession();
+  const { data: session, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <CircularProgress size={15} />;
+  }
+
   return (
     <Link
       className="flex gap-1"
       href={{ pathname: session ? "/account" : "/login" }}
     >
-      <Icon name="user" />
+      {session?.user.avatarUrl ? (
+        <div className="w-[28px] h-[28px] rounded-full border border-gray-400 p-[1px]">
+          <ApiImage
+            alt="user avatar"
+            src={session.user.avatarUrl}
+            className="w-full h-full object-cover rounded-full overflow-clip"
+          />
+        </div>
+      ) : (
+        <Icon name="user" />
+      )}
       {!!session && (
-        <span className="text-sm bg-gray-300 inline-block px-1 rounded hover:bg-gray-400">
-          {session.user?.firstName}
-        </span>
+        <Column
+          center
+          className="text-sm bg-gray-300 p-1 px-1.5 rounded hover:bg-gray-400 self-center"
+        >
+          <Caption size="md">{session.user?.firstName}</Caption>
+        </Column>
       )}
     </Link>
   );
