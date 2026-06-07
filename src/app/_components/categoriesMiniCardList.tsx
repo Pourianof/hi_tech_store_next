50302079ecb7b2327742b0725fc521074bef1ee1
Pooria Amini <pouriyaxamini@gmail.com>
2026-06-07 14:18:16 +0330

    wrap in link to category page

--- src/app/_components/categoriesMiniCardList.tsx
+++ src/app/_components/categoriesMiniCardList.tsx
@@ -2,6 +2,7 @@ import { getCategories } from "@/api/categoryApi";
 import { Category } from "@/core/models/category";
 import { ApiImage } from "@/ui/image/ApiImage";
 import { Slider, SliderContainer, SliderItem } from "@/ui/slider";
+import Link from "next/link";
 
 function CategoryMiniCard(props: { category: Category }) {
   return (
@@ -29,7 +30,16 @@ export async function CategoriesMiniCardList() {
       <SliderContainer className="gap-2 md:gap-4 my-4 items-stretch">
         {categories.items.map((cat) => (
           <SliderItem key={cat.categoryId} className="shrink-0 w-1/4 md:w-1/6">
-            <CategoryMiniCard category={cat} key={cat.categoryId} />
+            <Link
+              href={{
+                pathname: "/products",
+                query: {
+                  category: cat.categoryId,
+                },
+              }}
+            >
+              <CategoryMiniCard category={cat} key={cat.categoryId} />
+            </Link>
           </SliderItem>
         ))}
       </SliderContainer>
