e687fa3d629c3171dc3bd4e3a8fd2c42bfc879d1
Pooria Amini <pouriyaxamini@gmail.com>
2026-06-08 14:15:55 +0330

    handle window resize and adapt filler size

--- src/ui/fillerBox.tsx
+++ src/ui/fillerBox.tsx
@@ -4,30 +4,65 @@
  * good for mobile ui
  */
 
-import { ReactNode, useLayoutEffect, useRef } from "react";
+import {
+  ReactNode,
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+} from "react";
+import { useIsDesktopScreen } from "./theme/helpers/isDesktopMode";
 
 export function FillerBox({ children }: { children: ReactNode }) {
+  const isDesktopMode = useIsDesktopScreen();
   const containerRef = useRef<HTMLDivElement>(null);
+
+  const calculateWidth = useCallback(
+    function calculateWidth() {
+      if (!containerRef.current || isDesktopMode) {
+        return;
+      }
+
+      const { left, height } = containerRef.current.getBoundingClientRect();
+
+      // this strategy work better instead of using 100dvw because
+      // 100dvw include the scroll bar width which cause some y-basis
+      // overflow
+      containerRef.current.style.width = `calc(${
+        window.document.body.getBoundingClientRect().width
+      }px - ${left}px)`;
+
+      containerRef.current.parentElement!.style.height = `${height}px`;
+    },
+    [isDesktopMode],
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      calculateWidth();
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [calculateWidth]);
+
   useLayoutEffect(() => {
-    if (!containerRef.current) {
+    calculateWidth();
+  }, [calculateWidth]);
+
+  useEffect(() => {
+    if (!isDesktopMode || !containerRef.current) {
       return;
     }
 
-    const { left, height } = containerRef.current.getBoundingClientRect();
-
-    // this strategy work better instead of using 100dvw because
-    // 100dvw include the scroll bar width which cause some y-basis
-    // overflow
-    containerRef.current.style.width = `calc(${
-      window.document.body.getBoundingClientRect().width
-    }px - ${left}px)`;
-
-    containerRef.current.parentElement!.style.height = `${height}px`;
-  }, []);
+    containerRef.current.style.width = "auto";
+    containerRef.current.parentElement!.style.height = "auto";
+  }, [isDesktopMode]);
 
   return (
     <div className="relative w-full">
-      <div ref={containerRef} className="absolute top-0 left-0">
+      <div ref={containerRef} className="absolute desktop:static top-0 left-0">
         {children}
       </div>
     </div>
