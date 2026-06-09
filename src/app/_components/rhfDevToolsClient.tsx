541751593dfc7d05719ad506528738b6b88e36ca
Pooria Amini <pouriyaxamini@gmail.com>
2026-06-09 14:47:04 +0330

    fix production rendering of rhf-devtools

--- src/app/_components/rhfDevToolsClient.tsx
+++ src/app/_components/rhfDevToolsClient.tsx
@@ -1,8 +1,20 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { ReactNode } from "react";
-import { RhfDevTools } from "rhf-devtools";
+
+const RhfDevTools =
+  process.env.NODE_ENV == "development"
+    ? dynamic(async () => (await import("rhf-devtools")).RhfDevTools, {
+        ssr: false,
+      })
+    : null;
 
 export function RhfDevToolsClient({ children }: { children: ReactNode }) {
-  return <RhfDevTools>{children}</RhfDevTools>;
+  return (
+    process.env.NODE_ENV &&
+    RhfDevTools && (
+      <RhfDevTools displayOnlyIfAnyFormExists>{children}</RhfDevTools>
+    )
+  );
 }
