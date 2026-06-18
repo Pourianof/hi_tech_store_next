b535e6af50d7f9ee10383b89bb6c3a053f4a9881
Pooria Amini <pouriyaxamini@gmail.com>
2026-06-18 16:50:46 +0330

    fix authjs cookie name

--- src/lib/auth/sessionConsts.ts
+++ src/lib/auth/sessionConsts.ts
@@ -2,7 +2,7 @@ export const TOKEN_REFRESH_BUFFER = 5 * 60; // 5 minutes buffer before expiratio
 
 export const SESSION_SECURE = process.env.NEXTAUTH_URL?.startsWith("https://");
 export const SESSION_COOKIE = SESSION_SECURE
-  ? "__Secure-next-auth.session-token"
+  ? "__Secure-authjs.session-token"
   : "authjs.session-token";
 
 export const SESSION_TIMEOUT = 30 * 24 * 60 * 60; // 30 days
