404a059871b1fe3f1829a55c96f55b31576ac8e7
Pooria Amini <pouriyaxamini@gmail.com>
2026-06-20 19:13:35 +0330

    fix no cart exist bug

--- src/ui/contexts/cart/typeHelper/convertCartDtoToCartState.tsx
+++ src/ui/contexts/cart/typeHelper/convertCartDtoToCartState.tsx
@@ -3,34 +3,35 @@ import { Product, ProductVariation } from "@/core/models/product";
 import { CartItemsState, CartItemState } from "@/ui/contexts/cart/cartReducer";
 
 export function convertCartWithProductToCartState(
-  cart: CartWithProduct,
+  cart?: CartWithProduct,
 ): CartItemsState {
   return {
-    items: cart.items
-      .reduce<
-        {
-          variation: ProductVariation;
-          amount: number;
-          product: MinimalProductDto;
-        }[]
-      >(
-        (prev, cur) => [
-          ...prev,
-          ...cur.variations.map((v) => ({
-            variation: v.variation,
-            amount: v.amount,
-            product: cur,
-          })),
-        ],
-        [],
-      )
-      .map(
-        (item) =>
-          ({
-            amount: item.amount,
-            variation: item.variation,
-            product: item.product as unknown as Product,
-          }) as CartItemState,
-      ),
+    items:
+      cart?.items
+        .reduce<
+          {
+            variation: ProductVariation;
+            amount: number;
+            product: MinimalProductDto;
+          }[]
+        >(
+          (prev, cur) => [
+            ...prev,
+            ...cur.variations.map((v) => ({
+              variation: v.variation,
+              amount: v.amount,
+              product: cur,
+            })),
+          ],
+          [],
+        )
+        .map(
+          (item) =>
+            ({
+              amount: item.amount,
+              variation: item.variation,
+              product: item.product as unknown as Product,
+            }) as CartItemState,
+        ) ?? [],
   };
 }
