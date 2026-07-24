3899d282a81b462a920226257754d3461093fff6
Pooria Amini <pouriyaxamini@gmail.com>
2026-07-24 17:32:13 +0330

    style: improve error box

--- src/app/_components/safelyHealthyApiRender.tsx
+++ src/app/_components/safelyHealthyApiRender.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import { FailedBox } from "./failedBox";
 import { isHealthyApi } from "@/api/apiServerMisc";
+import { Column } from "@/ui/layouts/column";
 
 export async function SafelyHealthyApiRender({
   children,
@@ -12,9 +13,11 @@ export async function SafelyHealthyApiRender({
   return isServerHealthy ? (
     children
   ) : (
-    <FailedBox
-      title="We are down"
-      message="Server is not accessible for now. We will happy to see you soon some later"
-    />
+    <Column center className="h-dvh w-dvw">
+      <FailedBox
+        title="We are down"
+        message="Server is not accessible for now. We will happy to see you soon some later"
+      />
+    </Column>
   );
 }
